5c6a466a1dd52d05dde1cf468e894535ff67ae39
Joe Connolly <jcjconnolly4@gmail.com>
2019-05-02 23:03:28 -0400

    initial commit

---
+++ src/components/ViewCount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="count">#Todo Items: <strong>{{count}}</strong></div>
+</template>
+
+<script>
+export default {
+  name: "view-count",
+  props: ["count"],
+  methods: {
+  }
+};
+</script>
+
+
+<style>
+.count {
+  margin: 0px 10px;
+};
+</style>
